ba39210d8dc7b08c9d339b1e821a71dd78b16dfb
Nick Strauch <njs35@duke.edu>
2021-11-13 22:27:38 -0500

    Created GamePiece abstraction. This is the first stpe in removing conditional statements in thw drawing of the view board #9

--- src/ooga/view/gameDisplay/center/BoardView.java
+++ src/ooga/view/gameDisplay/center/BoardView.java
@@ -1,17 +1,25 @@
 package ooga.view.gameDisplay.center;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import ooga.view.gameDisplay.gamePieces.GamePiece;
+import ooga.view.gameDisplay.gamePieces.WallPiece;
 
 public class BoardView {
+  public static final int CELL_SIZE = 25;
   private GridPane myGrid;
+  private GamePiece myPiece;
   private static final int[][] fakeControllerBoard = {{0,0,0,0,0,0}, {0,1,1,1,1,0}, {0,0,0,1,0,0}, {0,1,1,2,1,0}, {0,1,0,1,1,0}, {0,0,0,0,0,0}};
 
   public BoardView(){
     myGrid = new GridPane();
-    myGrid.setGridLinesVisible(true);
+    myGrid.setMaxSize(CELL_SIZE, CELL_SIZE);
+    //myGrid.setGridLinesVisible(true);
+    myGrid.getStyleClass().add("gameGridPane");
   }
 
   public GridPane makeBoard(){
@@ -21,14 +29,19 @@ public class BoardView {
     for (int r = 0; r < 6; r++) { //Replace 6 with num rows and num cols
       for (int c = 0; c < 6; c++) {
         currentState = fakeControllerBoard[r][c]; //TODO replace with a myController.getState(r,c) method
+        //TODO Refactor conditionals out. Maybe make an abstraction of 'game pieces' then use reflection?
         if(currentState == 0){ //Wall
-          myGrid.add(new Rectangle(30,30), c, r);
+          myPiece = new WallPiece();  //example of using the GamePiece abstraction. Obviously still need to remove conditionals (replace with refection)
+          myGrid.add(myPiece.getPiece(), c, r);
         }
         if(currentState == 1){ //empty with dot pickup
-          myGrid.add(new Circle(5, Color.CORNFLOWERBLUE), c, r);
+          Circle dot = new Circle(5);
+          dot.getStyleClass().add("dotPickup");
+          myGrid.add(dot, c, r);
+          myGrid.setHalignment(dot, HPos.CENTER);
         }
         if(currentState == 2){ //Pacman
-          Circle testPacman = new Circle(15, Color.YELLOW);
+          Circle testPacman = new Circle(12.5, Color.YELLOW);
           myGrid.add(testPacman, c,r);
         }
       }
---
+++ src/ooga/view/gameDisplay/gamePieces/WallPiece.java
@@ -0,0 +1,24 @@
+package ooga.view.gameDisplay.gamePieces;
+
+import javafx.scene.Node;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * Class that represents the wall pieces in the view board.
+ */
+public class WallPiece extends GamePiece{
+  public static final int WALL_SIZE = 25;
+  private static final String CSS_ID = "wallPiece";
+
+
+  public WallPiece(){
+  }
+
+  @Override
+  protected Node makeNode(){
+    Rectangle wall = new Rectangle(WALL_SIZE, WALL_SIZE);
+    setIDs(wall, CSS_ID, getCellIndexID());
+    return wall;
+  }
+
+}
