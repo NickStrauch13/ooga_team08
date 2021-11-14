6dfc240c3fdce98f40c5d2603c472bb1508eaa1d
Neil Mosca <nam65@duke.edu>
2021-11-14 13:43:55 -0500

    updating my branch

--- src/ooga/view/home/HomeScreen.java
+++ src/ooga/view/home/HomeScreen.java
@@ -43,28 +43,18 @@ public class HomeScreen {
   public Scene setScene(){
     Scene scene = new Scene(root, myWidth, myHeight);
     scene.getStylesheets().add(getClass().getResource(DEFAULT_STYLESHEET).toExternalForm());
-    setupScene();
-    //Add css styling?
+    root.setCenter(homeButtons());
     return scene;
   }
 
-  private void setupScene() {
-    Node row = homeButtons();
-    root.setCenter(row);
-}
-
   private Node homeButtons(){
-    Button loadFileButton = myNodeBuilder.makeButton(myResources.getString("HighScores"),null, "homeScreenButton","highScoresButton",e -> readFile());
+    Button loadFileButton = myNodeBuilder.makeButton(myResources.getString("HighScores"),null, "homeScreenButton","highScoresButton",e -> viewHighScores());
     Button newGameButton = myNodeBuilder.makeButton(myResources.getString("NewGame"), null,"homeScreenButton","newGameButton",e -> startNewGame());
     Label inputText = myNodeBuilder.makeLabel("userNameText");
     TextField userName = myNodeBuilder.makeInputField("userName", e -> setUserName(e), "");
     Node row1 = myNodeBuilder.makeRow("homeColFormat", loadFileButton, newGameButton);
     Node row2 = myNodeBuilder.makeRow("homeColFormat", inputText, userName);
     return myNodeBuilder.makeCol("homeRowFormat", row1, row2);
-  }
-
-  public void setUserName(String userName) {
-    this.userName = userName;
   }
 
   private void readFile(){
@@ -79,8 +69,17 @@ public class HomeScreen {
   }
 
   private void startNewGame() {
+    readFile();
     GameDisplay gameDisplay = new GameDisplay(myWidth, myHeight, "Default", language,  "Pacman");
     gameDisplay.setMainDisplay(myStage, "Pacman");
+  }
+
+  private void viewHighScores() {
+    System.out.println("High Scores");
+  }
+
+  private void setUserName(String userName) {
+    this.userName = userName;
   }
 
 
