b45c4e369c7f8cc1dc27d794872d825622a16c06
Neil Mosca <nam65@duke.edu>
2021-11-15 00:44:20 -0500

    got view to properly draw parsed data file input at the HomeScreen #22 #23

--- src/ooga/view/gameDisplay/center/BoardView.java
+++ src/ooga/view/gameDisplay/center/BoardView.java
@@ -14,43 +14,47 @@ public class BoardView {
   public static final int CELL_SIZE = 25;
   private GridPane myGrid;
   private GamePiece myPieces;
-  private static final int[][] fakeControllerBoard = {{0,0,0,0,0,0}, {0,1,1,1,1,0}, {0,0,0,1,0,0}, {0,1,1,2,1,0}, {0,1,0,1,1,0}, {0,0,0,0,0,0}};
+  private int[][] controllerBoard;
 
   public BoardView(){
     myGrid = new GridPane();
     myGrid.setMaxSize(CELL_SIZE, CELL_SIZE);
     //myGrid.setGridLinesVisible(true);
     myGrid.getStyleClass().add("gameGridPane");
+
   }
 
-  public GridPane makeBoard(){
-    //TODO get board data from the controller
-    //TODO Refactor conditionals out. Maybe make an abstraction of 'game pieces' then use reflection?
-    int currentState; //may actually want this as a string
-    for (int r = 0; r < 6; r++) { //Replace 6 with num rows and num cols
-      for (int c = 0; c < 6; c++) {
-        currentState = fakeControllerBoard[r][c]; //TODO replace with a myController.getState(r,c) method
-        //TODO Refactor conditionals out. Maybe make an abstraction of 'game pieces' then use reflection?
-        if(currentState == 0){ //Wall
-          myPieces = new WallPiece();  //example of using the GamePiece abstraction. Obviously still need to remove conditionals (replace with refection)
-          myGrid.add(myPieces.getPiece(), c, r);
-        }
-        if(currentState == 1){ //empty with dot pickup
-          myPieces = new DotPiece();
-          Node dot = myPieces.getPiece();
-          myGrid.add(dot, c, r);
-          myGrid.setHalignment(dot, HPos.CENTER);
-        }
-        if(currentState == 2){ //Pacman
-          myPieces = new PacmanPiece();
-          Node pacman = myPieces.getPiece();
-          myGrid.add(pacman, c,r);
-          myGrid.setHalignment(pacman, HPos.CENTER);
-        }
+  public void addBoardPiece(int row, int col, String objectName) {
+
+      if(objectName.equals("WALL")){ //Wall
+        System.out.println(objectName);
+        myPieces = new WallPiece();  //example of using the GamePiece abstraction. Obviously still need to remove conditionals (replace with refection)
+        myGrid.add(myPieces.getPiece(), col, row);
       }
-    }
+      if(objectName.equals("POWERUP1")){ //empty with dot pickup
+        System.out.println(objectName);
+        myPieces = new DotPiece();
+        Node dot = myPieces.getPiece();
+        myGrid.add(dot, col, row);
+        myGrid.setHalignment(dot, HPos.CENTER);
+      }
 
+    /*
+    if(currentState == 2){ //Pacman
+      myPieces = new PacmanPiece();
+      Node pacman = myPieces.getPiece();
+      myGrid.add(pacman, c,r);
+      myGrid.setHalignment(pacman, HPos.CENTER);
+    }*/
 
+  }
+
+  public void makeBoard(int rows, int cols){
+    controllerBoard = new int[rows][cols];
+  }
+
+  public GridPane getInitialBoard() {
     return myGrid;
   }
+
 }
