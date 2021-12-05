8cd42f0b84e9e791a4dfceaf870fd76166163d2c
Nick Strauch <njs35@duke.edu>
2021-12-05 17:56:57 -0500

    fixed game pieces. Now board is more stable. #47

--- src/ooga/view/gameDisplay/gamePieces/PortalPiece.java
+++ src/ooga/view/gameDisplay/gamePieces/PortalPiece.java
@@ -9,12 +9,16 @@ import java.util.Map;
  * Class that represents the dot pickup pieces in the view board.
  */
 public class PortalPiece extends GamePiece{
-  public int dotRadius = 8;
-  public Color myColor=Color.ORANGE;
+  private static final int DEFAULT_RAD = 8;
+  private int dotRadius;
+  private static final Color DEFAULT_COLOR = Color.ORANGE;
+  private Color myColor;
   private static final String CSS_ID = "dotPiece";
 
   public PortalPiece(Integer cellSize, Map<String, String> myValues){
     super(cellSize);
+    dotRadius = DEFAULT_RAD;
+    myColor = DEFAULT_COLOR;
     if (myValues != null) {
       if(myValues.containsKey("POWERUP_COLOR")){
         String rgbValues= myValues.get("POWERUP_COLOR"); //TODO PARSE OUT NEGATIVE DATA
